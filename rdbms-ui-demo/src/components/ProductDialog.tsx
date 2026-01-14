05663f12f5be40ca0b6996441f5d8e32b4bb9b3a
muasya-victor <vicmwe184@gmail.com>
2026-01-14 20:28:49 +0300

    - currency update

--- rdbms-ui-demo/src/components/ProductDialog.tsx
+++ rdbms-ui-demo/src/components/ProductDialog.tsx
@@ -170,7 +170,7 @@ export default function ProductDialog({
 
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-2">
-              <Label htmlFor="price">Price ($) *</Label>
+              <Label htmlFor="price">Price (KES) *</Label>
               <Input
                 id="price"
                 type="number"
